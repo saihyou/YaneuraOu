d8195f363bbb4bb7987b7b43f0c265cb64ba0195
saihyou <you_hey81@yahoo.co.jp>
2024-03-12 00:49:37 +0900

    fix: revert change for dr2

--- source/usi_option.cpp
+++ source/usi_option.cpp
@@ -71,7 +71,7 @@ namespace USI {
 		// Stockfishもこうすべきだと思う。
 
 #if !defined(__EMSCRIPTEN__)
-		o["Threads"] << Option(24, 1, 1024, [](const Option& o) { /* on_threads(o); */ });
+		o["Threads"] << Option(4, 1, 1024, [](const Option& o) { /* on_threads(o); */ });
 #else
 		// yaneuraou.wasm
 		// スレッド数などの調整
@@ -82,7 +82,7 @@ namespace USI {
 
 #if !defined(TANUKI_MATE_ENGINE) && !defined(YANEURAOU_MATE_ENGINE)
 		// 置換表のサイズ。[MB]で指定。
-		o["USI_Hash"] << Option(98304, 1, MaxHashMB, [](const Option& o) { /* on_hash_size(o); */ });
+		o["USI_Hash"] << Option(1024, 1, MaxHashMB, [](const Option& o) { /* on_hash_size(o); */ });
 
 #if defined(USE_EVAL_HASH)
 		// 評価値用のcacheサイズ。[MB]で指定。
